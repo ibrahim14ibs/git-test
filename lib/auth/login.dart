86c9d86f91924effd6345552ba2198bf05794561
ibrahim14ibs <ibrahim14ibs@gmail.com>
2022-08-27 12:03:17 +0300

    Fix Error In Login Page

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -1,21 +1,18 @@
-class LoginPage extends StatefulWidget {
+import 'package:flutter/material.dart';
 
+class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
- 
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-        appBar: AppBar(
-        
-          title: Text("Login"),
-        ),
-        body: Center(),
-        
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Center(),
+    );
   }
 }
